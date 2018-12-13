6db27ec79b7ca83fac48e8fd0c2a2030fb44f7d3
Themi <Tcm4760@gmail.com>
2018-12-13 13:24:11 -0600

    f

--- app/src/main/java/com/example/tmeg2101/test/MainActivity.java
+++ app/src/main/java/com/example/tmeg2101/test/MainActivity.java
@@ -23,14 +23,14 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+    //    FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+    //    fab.setOnClickListener(new View.OnClickListener() {
+    //        @Override
+    //        public void onClick(View view) {
+    //            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+    //                    .setAction("Action", null).show();
+    //        }
+    //    });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -83,7 +83,7 @@ public class MainActivity extends AppCompatActivity
         if (id == R.id.nav_camera) {
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
-
+            
         } else if (id == R.id.nav_slideshow) {
 
         } else if (id == R.id.nav_manage) {
