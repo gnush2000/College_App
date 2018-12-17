4a2909b5de34ecc2ff00f61491b45ce538a7e70b
Themi <Tcm4760@gmail.com>
2018-12-17 13:04:00 -0600

    Update MainActivity.java

--- app/src/main/java/com/example/tmeg2101/test/MainActivity.java
+++ app/src/main/java/com/example/tmeg2101/test/MainActivity.java
@@ -80,17 +80,15 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_profile) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_family) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_attended) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_essay) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_video) {
 
         }
 
