70327f14d1c0ed87dee799fa4f6dad18c465931c
Themi <Tcm4760@gmail.com>
2019-01-09 13:10:43 -0600

    asd

--- app/src/main/java/com/example/tmeg2101/test/NavProfile.java
+++ app/src/main/java/com/example/tmeg2101/test/NavProfile.java
@@ -2,6 +2,7 @@ package com.example.tmeg2101.test;
 
 
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,11 +12,13 @@ import android.widget.Button;
 
 
 
-public class NavProfile extends android.support.v4.app.Fragment{
+public class NavProfile extends android.support.v4.app.Fragment {
     private Button Submit;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.nav_profile, container, false);
+        super.onCreate(savedInstanceState);
         Submit = (Button) findViewById(R.id.submitProfileButton);
         Submit.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
