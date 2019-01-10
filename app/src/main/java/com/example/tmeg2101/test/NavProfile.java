009a40ef4e07035dd092cc6be8f62537a39edfbf
Themi <Tcm4760@gmail.com>
2019-01-10 13:45:26 -0600

    buttion works!

--- app/src/main/java/com/example/tmeg2101/test/NavProfile.java
+++ app/src/main/java/com/example/tmeg2101/test/NavProfile.java
@@ -8,21 +8,32 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 
 
 
 
 public class NavProfile extends android.support.v4.app.Fragment {
     private Button Submit;
-
+    private static final String TAG =
+            NavProfile.class.getName();
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.nav_profile, container, false);
         super.onCreate(savedInstanceState);
-        Submit = (Button) findViewById(R.id.submitProfileButton);
+        Submit = (Button) view.findViewById(R.id.submitProfileButton);
+        //text
+        EditText first = (EditText) view.findViewById(R.id.editName);
+        EditText last = (EditText) view.findViewById(R.id.editLast);
+        EditText dob = (EditText) view.findViewById(R.id.editName);
+        EditText study = (EditText) view.findViewById(R.id.editName);
+        EditText address = (EditText) view.findViewById(R.id.editName);
+
+
+
         Submit.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-
+                Log.i(TAG, "First name is: " + first.getText().toString());
             }
         });
         return view;
