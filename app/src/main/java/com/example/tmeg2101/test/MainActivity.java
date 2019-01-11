1c129c833b7461f3cdf3175c01ad36c632cb3e63
Themi <Tcm4760@gmail.com>
2019-01-11 12:59:24 -0600

    df

--- app/src/main/java/com/example/tmeg2101/test/MainActivity.java
+++ app/src/main/java/com/example/tmeg2101/test/MainActivity.java
@@ -14,6 +14,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.support.v4.app.Fragment;
+import com.backendless.Backendless;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -24,6 +25,9 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        //backendless
+        Backendless.initApp(getApplicationContext(), , api-key );
 
     //    FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
     //    fab.setOnClickListener(new View.OnClickListener() {
