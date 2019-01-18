1b41b672e8f5b45829ea1f7aa36255b36674f5d6
Themi <Tcm4760@gmail.com>
2019-01-18 13:10:37 -0600

    upload

--- app/src/main/java/com/example/tmeg2101/test/NavEssay.java
+++ app/src/main/java/com/example/tmeg2101/test/NavEssay.java
@@ -1,6 +1,7 @@
 package com.example.tmeg2101.test;
 
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.jaiselrahman.filepicker.activity.FilePickerActivity;
 import com.jaiselrahman.filepicker.config.Configurations;
@@ -16,7 +18,10 @@ import com.jaiselrahman.filepicker.model.MediaFile;
 import java.io.File;
 import java.util.ArrayList;
 
+import static com.backendless.rt.RTTypes.log;
+
 public class NavEssay extends android.support.v4.app.Fragment{
+    int FILE_REQUEST_CODE = 42;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.nav_essay, container, false);
@@ -27,15 +32,12 @@ public class NavEssay extends android.support.v4.app.Fragment{
                 Log.i("Submit"," click");
 
                 Intent intent = new Intent(NavEssay.this.getActivity(), FilePickerActivity.class);
-                int FILE_REQUEST_CODE = 42;
-                startActivityForResult(intent, FILE_REQUEST_CODE);
+                intent.putExtra(FilePickerActivity.CONFIGS, new Configurations.Builder()
+                        .setMaxSelection(1)
+                        .enableImageCapture(true)
+                        .build());
 
-                switch (FILE_REQUEST_CODE) {
-                    case 42:
-                        ArrayList<MediaFile> files = data.getParcelableArrayListExtra(FilePickerActivity.MEDIA_FILES);
-                        //Do something with files
-                        break;
-                }
+                startActivityForResult(intent, FILE_REQUEST_CODE);
 
             }
         });
@@ -47,13 +49,12 @@ public class NavEssay extends android.support.v4.app.Fragment{
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         try {
             super.onActivityResult(requestCode, resultCode, data);
+            ArrayList<MediaFile> files = data.getParcelableArrayListExtra(FilePickerActivity.MEDIA_FILES);
 
-            if (requestCode == FILE_REQUEST_CODE  && resultCode  == RESULT_OK) {
+            Log.i("selected file: ", files.get(0).getName());
 
-                String requiredValue = data.getStringExtra("key");
-            }
         } catch (Exception ex) {
-            Toast.makeText(Activity.this, ex.toString(),
+            Toast.makeText(NavEssay.this.getActivity(), ex.toString(),
                     Toast.LENGTH_SHORT).show();
         }
 
